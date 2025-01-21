f7a9a109c3af15a974b23f3623ba387152acd71e
Ivan <taktaef@yandex.ru>
2025-01-21 22:30:37 +0300

    fix proton dir

--- base.sh
+++ base.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 notify-send "proton-GE-Latest install started"
+rm -rf /tmp/Proton-GE-Latest
 mkdir -p /tmp/Proton-GE-Latest
 cd /tmp/Proton-GE-Latest
 wget $(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | sed -n '2p')
 archive=$(ls | grep 'tar')
 mkdir -p ~/.steam/root/compatibilitytools.d/Proton-GE-Latest
-tar -xvf $archive  --overwrite-dir --recursive-unlink -C ~/.steam/root/compatibilitytools.d/Proton-GE-Latest
-rm /tmp/Proton-GE-Latest
-notify-send "proton-GE-Latest install finished"
+tar -xvf $archive
+cp -rf ./${archive::-7}/* ~/.steam/root/compatibilitytools.d/Proton-GE-Latest/
+rm -rf /tmp/Proton-GE-Latest
+notify-send "${archive::-7} install finished"
