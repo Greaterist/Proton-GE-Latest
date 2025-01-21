4a559abf2a4f95626a74ccf66bfa6417937c1e15
Ivan <taktaef@yandex.ru>
2025-01-21 23:07:06 +0300

    fix steam proton name

--- base.sh
+++ base.sh
@@ -5,8 +5,9 @@ mkdir -p /tmp/Proton-GE-Latest
 cd /tmp/Proton-GE-Latest
 wget $(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | sed -n '2p')
 archive=$(ls | grep 'tar')
-mkdir -p ~/.local/share/Steam/compatibilitytools.d//Proton-GE-Latest
+mkdir -p ~/.local/share/Steam/compatibilitytools.d/Proton-GE-Latest
 tar -xvf $archive
-cp -rf ./${archive::-7}/* ~/.local/share/Steam/compatibilitytools.d//Proton-GE-Latest/
+cp -rf ./${archive::-7}/* ~/.local/share/Steam/compatibilitytools.d/Proton-GE-Latest/
 rm -rf /tmp/Proton-GE-Latest
+sed -i '/"display_name"/c\      "display_name" "Proton-GE-Latest"' ~/.local/share/Steam/compatibilitytools.d/Proton-GE-Latest/compatibilitytool.vdf
 notify-send "${archive::-7} install finished"
