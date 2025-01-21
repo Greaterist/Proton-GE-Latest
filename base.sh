99a8df4d8b4f38172cca310b28c86aafc18483dc
Ivan <taktaef@yandex.ru>
2025-01-21 21:40:13 +0300

    add start notification

--- base.sh
+++ base.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+notify-send "proton-GE-Latest install started"
 mkdir -p /tmp/Proton-GE-Latest
 cd /tmp/Proton-GE-Latest
 wget $(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | sed -n '2p')
