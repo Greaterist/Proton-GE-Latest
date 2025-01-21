54783f08f260d36fc597ff3b4da4e7d83da68a45
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2025-01-21 21:07:27 +0300

    Add files via upload

---
+++ base.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p /tmp/Proton-GE-Latest
+cd /tmp/Proton-GE-Latest
+wget $(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | sed -n '2p')
+archive=$(ls | grep 'tar')
+mkdir -p ~/.steam/root/compatibilitytools.d/Proton-GE-Latest
+tar -xvf $archive  --overwrite-dir --recursive-unlink -C ~/.steam/root/compatibilitytools.d/Proton-GE-Latest
+rm /tmp/Proton-GE-Latest
+
